be4bf4e20b1792626bd454abebb2678b18929a6c
razvanbaboiucs <razvanbaboiu@gmail.com>
2024-11-30 19:39:33 +0200

    format file

--- src/middleware/authenticated.ts
+++ src/middleware/authenticated.ts
@@ -10,53 +10,51 @@ import { RoleType } from "../types/roles";
 import { AppContext } from "../types/appContext";
 
 export const authenticated = createMiddleware(async (c, next) => {
-    const token = c.req.header("Authorization")?.split(" ")[1]; // Authorization: Bearer <token>
+  const token = c.req.header("Authorization")?.split(" ")[1]; // Authorization: Bearer <token>
+  if (!token) {
+    console.error("No token provided");
+    throw new HTTPException(401, { message: "Unauthorized" });
+  }
 
-    if (!token) {
-        console.error("No token provided");
-        throw new HTTPException(401, { message: "Unauthorized" });
-    }
+  const payload = await getPayloadFromToken(token, c);
+  if (!payload?.email) {
+    console.error("Invalid token");
+    throw new HTTPException(401, { message: "Unauthorized: Invalid token" });
+  }
 
-    const payload = await getPayloadFromToken(token, c);
+  const db = drizzle(c.env.DB);
+  const user = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, payload.email));
+  if (user.length === 0) {
+    console.error("User not found");
+    throw new HTTPException(401, { message: "Unauthorized" });
+  }
+  const currentUser = {
+    id: user[0].id,
+    email: user[0].email,
+    role: user[0].role as RoleType,
+  };
 
-    if (!payload?.email) {
-        console.error("Invalid token");
-        throw new HTTPException(401, { message: "Unauthorized: Invalid token" });
-    }
+  c.set("user", { ...currentUser }); // set current user in context
 
-    const db = drizzle(c.env.DB);
-    const user = await db.select().from(users).where(eq(users.email, payload.email));
-
-    if (user.length === 0) {
-        console.error("User not found");
-        throw new HTTPException(401, { message: "Unauthorized" });
-    }
-
-    const currentUser = {
-        id: user[0].id,
-        email: user[0].email,
-        role: user[0].role as RoleType
-    };
-
-    c.set("user", {...currentUser}); // set current user in context
-
-    await next();
+  await next();
 });
 
 async function getPayloadFromToken(token: string, c: AppContext) {
-    try {
-        return await verify(token, c.env.JWT_SECRET) as UserJwtPayload;
+  try {
+    return (await verify(token, c.env.JWT_SECRET)) as UserJwtPayload;
+  } catch (error) {
+    if (error instanceof JwtTokenInvalid) {
+      console.error("Invalid token");
+      throw new HTTPException(401, { message: "Unauthorized: Invalid token" });
     }
-    catch (error) {
-        if (error instanceof JwtTokenInvalid) {
-            console.error("Invalid token");
-            throw new HTTPException(401, { message: "Unauthorized: Invalid token" });
-        }
-        if (error instanceof JwtTokenExpired) {
-            console.error("Token expired");
-            throw new HTTPException(401, { message: "Unauthorized: Token expired" });
-        }
-        console.error("Error while authenticating user", error);
-        throw new HTTPException(401, { message: "Unauthorized" });
+    if (error instanceof JwtTokenExpired) {
+      console.error("Token expired");
+      throw new HTTPException(401, { message: "Unauthorized: Token expired" });
     }
+    console.error("Error while authenticating user", error);
+    throw new HTTPException(401, { message: "Unauthorized" });
+  }
 }
